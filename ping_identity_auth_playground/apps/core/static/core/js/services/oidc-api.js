efa1da81bb75e078edc2b4842b249fd94051548a
Willian Antunes <willian.lima.antunes@gmail.com>
2021-11-29 14:59:32 -0300

    chore: change to app.local instead of localhost

--- ping_identity_auth_playground/apps/core/static/core/js/services/oidc-api.js
+++ ping_identity_auth_playground/apps/core/static/core/js/services/oidc-api.js
@@ -5,7 +5,7 @@ export async function consultUserData() {
     method: "GET",
     timeout: 15 * 1000,
   }
-  options.url = "https://localhost:9443/api/v1/user-info"
+  options.url = "https://app.local:9443/api/v1/user-info"
   options.headers = { ...options.headers }
 
   const response = await axios(options)
